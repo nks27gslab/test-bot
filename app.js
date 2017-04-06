b5e9ff3e03a64d1b5095283e707c0ddcd234c7f8
Neelamkumar Sharma <neelamkumar.sharma@gmail.com>
2017-04-06 18:21:39 +0530

    modules checkin

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/index.html');
 });
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+io.on('connection', function(socket){
+ socket.on('chat message', function(msg){
+ io.emit('chat message', msg);
+ });
 });
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+http.listen(port, function(){
+ console.log('listening on ' + port);
 });
